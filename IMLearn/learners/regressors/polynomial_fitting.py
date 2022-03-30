bc13085123850167f7f4fa277f7fe7f5bee6cc0d
Assaf Zaitlin <assaty@gmail.com>
2022-03-30 10:50:42 +0300

    added secind practical part ex2

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -9,7 +9,7 @@ class PolynomialFitting(BaseEstimator):
     """
     Polynomial Fitting using Least Squares estimation
     """
-    def __init__(self, k: int) -> PolynomialFitting:
+    def __init__(self, k: int):
         """
         Instantiate a polynomial fitting estimator
 
@@ -19,7 +19,8 @@ class PolynomialFitting(BaseEstimator):
             Degree of polynomial to fit
         """
         super().__init__()
-        raise NotImplementedError()
+        self.k = k
+        self.linear_model = LinearRegression(include_intercept=False)
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -33,7 +34,13 @@ class PolynomialFitting(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        if len(X.shape) > 1:
+            if X.shape[1] == 1:
+                X = np.array([i[0] for i in X])
+            else:
+                raise ValueError("Bad dimensions")
+        vandermonde = self.__transform(X)
+        self.linear_model.fit(vandermonde, y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -49,7 +56,13 @@ class PolynomialFitting(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        if len(X.shape) > 1:
+            if X.shape[1] == 1:
+                X = np.array([i[0] for i in X])
+            else:
+                raise ValueError("Bad dimensions")
+        vandermonde = self.__transform(X)
+        return self.linear_model.predict(vandermonde)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -68,7 +81,13 @@ class PolynomialFitting(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        if len(X.shape) > 1:
+            if X.shape[1] == 1:
+                X = np.array([i[0] for i in X])
+            else:
+                raise ValueError("Bad dimensions")
+        vandermonde = self.__transform(X)
+        return self.linear_model.loss(vandermonde, y)
 
     def __transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -83,4 +102,4 @@ class PolynomialFitting(BaseEstimator):
         transformed: ndarray of shape (n_samples, k+1)
             Vandermonde matrix of given samples up to degree k
         """
-        raise NotImplementedError()
+        return np.vander(X, self.k + 1)
--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -1,13 +1,23 @@
-import IMLearn.learners.regressors.linear_regression
 from IMLearn.learners.regressors import PolynomialFitting
+import IMLearn.learners.regressors.linear_regression
 from IMLearn.utils import split_train_test
-
-import numpy as np
-import pandas as pd
 import plotly.express as px
 import plotly.io as pio
-pio.templates.default = "simple_white"
+import pandas as pd
+import numpy as np
 
+pio.templates.default = "simple_white"
+DATA_PATH = '../datasets/City_Temperature.csv'
+DATE_COL_INDEX = 2
+DATE_COL = 'Date'
+MONTH_COL = 'Month'
+YEAR_COL = 'Year'
+COUNTRY_COL = 'Country'
+DAY_OF_YEAR_COL = 'DayOfYear'
+CITY_COL = 'City'
+DAY_COL = 'Day'
+TEMP_COL = 'Temp'
+COUNTRIES = ['Jordan', 'The Netherlands', 'South Africa']
 
 def load_data(filename: str) -> pd.DataFrame:
     """
@@ -21,22 +31,68 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
-
+    df = pd.read_csv(filename, parse_dates=[DATE_COL_INDEX])
+    df[DAY_OF_YEAR_COL] = df[DATE_COL].apply(lambda x: x.dayofyear)
+    # month_dummies = pd.get_dummies(df[MONTH_COL])
+    # df = pd.concat([df, month_dummies], axis=1)
+    df.drop([DATE_COL, CITY_COL, DAY_COL], axis=1,
+            inplace=True)
+    df = df[(df[TEMP_COL] < 50) & (df[TEMP_COL] > -15)]
+    return df
 
 if __name__ == '__main__':
-    np.random.seed(0)
+    np.random.seed(115)
+
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
+    df = load_data(DATA_PATH)
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
+    israel_df = df[df[COUNTRY_COL] == "Israel"]
+    # israel_df[YEAR_COL] = israel_df[YEAR_COL].astype(str)
+    # px.scatter(israel_df, x=DAY_OF_YEAR_COL, y=TEMP_COL,
+    #            color=israel_df[YEAR_COL].astype(str),
+    #            title="Temperature by day of year").show()
+    std_per_month = israel_df.groupby(MONTH_COL).agg(np.std)
+    # px.bar(std_per_month, x=std_per_month.index, y=TEMP_COL,
+    #        labels={TEMP_COL: 'Standard deviation of temperature'},
+    #        title="Standard deviation of temperature each month").show()
 
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
+    grouped_by = df.groupby([COUNTRY_COL, MONTH_COL]).agg({TEMP_COL: [np.mean, np.std]})
+    grouped_by.columns = ['Temp mean', 'Temp std']
+    grouped_by.reset_index(inplace=True)
+    # px.line(grouped_by, x=MONTH_COL,
+    #         y="Temp mean",
+    #         color=COUNTRY_COL,
+    #         error_y="Temp std").show()
 
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+    israel_X_df = israel_df[[DAY_OF_YEAR_COL]]
+    errors = []
+    for k in range(1, 11):
+        train_X, train_y, test_X, test_y = split_train_test(israel_X_df,
+                                                            israel_df[TEMP_COL])
+        model = PolynomialFitting(k)
+        model.fit(train_X.to_numpy(), train_y.to_numpy())
+        error = model.loss(test_X.to_numpy(), test_y.to_numpy())
+        print(f"error for k={k}: {error}")
+        errors.append((k, error))
+    error_df = pd.DataFrame(errors, columns=['Polynomial degree', 'Error'])
+    # px.bar(error_df, x="Polynomial degree", y='Error',
+    #        title='Error in prediction by polynomial degree').show()
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+    model = PolynomialFitting(4)
+    train_X, train_y = israel_X_df, israel_df[TEMP_COL]
+    model.fit(train_X.to_numpy(), train_y.to_numpy())
+    error_by_country = []
+    for country in COUNTRIES:
+        country_df = df[df[COUNTRY_COL] == country]
+        test_X = country_df[[DAY_OF_YEAR_COL]]
+        test_y = country_df[TEMP_COL]
+        error = model.loss(test_X.to_numpy(), test_y.to_numpy())
+        error_by_country.append((country, error))
+    country_error_df = pd.DataFrame(error_by_country,
+                                    columns=['Country', 'Error'])
+    px.bar(country_error_df, x='Country', y='Error',
+           title='Error in prediction by country').show()

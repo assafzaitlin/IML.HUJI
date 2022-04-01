56249a888e3f12f99becdf4b89939cf03d2b3569
Assaf Zaitlin <assaty@gmail.com>
2022-04-01 18:06:04 +0300

    changed code to create correct matrix

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -102,4 +102,4 @@ class PolynomialFitting(BaseEstimator):
         transformed: ndarray of shape (n_samples, k+1)
             Vandermonde matrix of given samples up to degree k
         """
-        return np.vander(X, self.k + 1)
+        return np.vander(X, self.k + 1, increasing=True)

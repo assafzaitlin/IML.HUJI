8065080a947090bb6bc74efa978cdd66d9d95669
Assaf Zaitlin <assaty@gmail.com>
2022-06-18 09:13:07 +0300

    added most parts of exercise

--- exercises/gradient_descent_investigation.py
+++ exercises/gradient_descent_investigation.py
@@ -7,8 +7,11 @@ from IMLearn.desent_methods import GradientDescent, FixedLR, ExponentialLR
 from IMLearn.desent_methods.modules import L1, L2
 from IMLearn.learners.classifiers.logistic_regression import LogisticRegression
 from IMLearn.utils import split_train_test
+from IMLearn.model_selection.cross_validate import cross_validate
+from IMLearn.metrics.loss_functions import misclassification_error
 
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 def plot_descent_path(module: Type[BaseModule],
@@ -73,25 +76,86 @@ def get_gd_state_recorder_callback() -> Tuple[Callable[[], None], List[np.ndarra
     weights: List[np.ndarray]
         Recorded parameters
     """
-    raise NotImplementedError()
+    values = []
+    weights = []
 
+    def callback(**kwargs):
+        weights.append(kwargs['weights'])
+        values.append(kwargs['val'])
+
+    return callback, values, weights
+
+
+def plot_delta(values, title, names=None):
+    if names is None:
+        scatters = [go.Scatter(y=val, x=list(range(len(val))), mode='lines')
+                    for val in values]
+    else:
+        scatters = [go.Scatter(y=values[i], x=list(range(len(values[i]))),
+                               name=names[i], mode='lines')
+                    for i in range(len(values))]
+    fig = go.Figure(scatters, layout=go.Layout(title=title))
+    fig.update_xaxes(title="Num of iterations").update_yaxes(title='Loss')
+    return fig
 
 def compare_fixed_learning_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
                                  etas: Tuple[float] = (1, .1, .01, .001)):
-    raise NotImplementedError()
-
+    losses = np.zeros((len(etas), 2))
+    for i, eta in enumerate(etas):
+        lr = FixedLR(eta)
+        callback, values, weights = get_gd_state_recorder_callback()
+        l1 = L1(init)
+        gd = GradientDescent(learning_rate=lr, callback=callback)
+        gd.fit(l1, None, None)
+        plot = plot_descent_path(module=L1, descent_path=np.array(weights),
+                                 title=f"L1 descent path with eta: {eta}")
+        plot.show()
+        plot2 = plot_delta([values], f"L1 deltas with eta: {eta}")
+        plot2.show()
+        min_loss = np.min(values)
+        losses[i][0] = min_loss
+        values.clear()
+        weights.clear()
+        l2 = L2(init)
+        gd.fit(l2, None, None)
+        plot = plot_descent_path(module=L2, descent_path=np.array(weights),
+                                 title=f"L2 descent path with eta: {eta}")
+        plot.show()
+        plot2 = plot_delta([values], f"L2 deltas with eta: {eta}")
+        plot2.show()
+        min_loss = np.min(values)
+        losses[i][1] = min_loss
+    print(losses)
 
 def compare_exponential_decay_rates(init: np.ndarray = np.array([np.sqrt(2), np.e / 3]),
                                     eta: float = .1,
                                     gammas: Tuple[float] = (.9, .95, .99, 1)):
     # Optimize the L1 objective using different decay-rate values of the exponentially decaying learning rate
-    raise NotImplementedError()
+    vals = []
+    min_loss = np.zeros(len(gammas))
+    decision_surface = None
+    for i, gamma in enumerate(gammas):
+        lr = ExponentialLR(eta, gamma)
+        callback, values, weights = get_gd_state_recorder_callback()
+        l1 = L1(init)
+        gd = GradientDescent(learning_rate=lr, callback=callback)
+        gd.fit(l1, None, None)
+        vals.append(values)
+        min_loss[i] = np.min(values)
+        if gamma == 0.95:
+            decision_surface = plot_descent_path(module=L1,
+                                                 descent_path=np.array(weights),
+                                                 title="descent path with "
+                                                       "gamma=0.95")
+    names = [f"gamma: {gamma}" for gamma in gammas]
+    fig = plot_delta(vals, "Loss as num of iterations for all gammas", names)
+    fig.show()
 
     # Plot algorithm's convergence for the different values of gamma
-    raise NotImplementedError()
+    print(min_loss)
 
     # Plot descent path for gamma=0.95
-    raise NotImplementedError()
+    decision_surface.show()
 
 
 def load_data(path: str = "../datasets/SAheart.data", train_portion: float = .8) -> \
@@ -131,15 +195,52 @@ def fit_logistic_regression():
     X_train, y_train, X_test, y_test = load_data()
 
     # Plotting convergence rate of logistic regression over SA heart disease data
-    raise NotImplementedError()
+    alphas = np.linspace(0, 1, 101)
+    model = LogisticRegression()
+    model.fit(X=X_train.to_numpy(), y=y_train.to_numpy())
+    pred = model.predict_proba(X_train.to_numpy())
+    tpr = []
+    fpr = []
+    diff = []
+    for alpha in alphas:
+        y_pred = np.where(pred >= alpha, 1, 0)
+        fp = np.sum((y_pred == 1) & (y_train == 0))
+        tp = np.sum((y_pred == 1) & (y_train == 1))
+        fn = np.sum((y_pred == 0) & (y_train == 1))
+        tn = np.sum((y_pred == 0) & (y_train == 0))
+        fpr_calc = fp / (fp + tn)
+        tpr_calc = tp / (tp + fn)
+        fpr.append(fpr_calc)
+        tpr.append(tpr_calc)
+        diff.append(tpr_calc - fpr_calc)
+    scatter = go.Scatter(x=fpr, y=tpr, mode='lines')
+    fig = go.Figure([scatter], layout=go.Layout(title="ROC for all alpha values"))
+    fig.update_xaxes(title="False positive").update_yaxes(title="True positive")
+    # fig.show()
+    best_alpha = int(np.argmax(diff)) / 100
+    print(f"best alpha is: {best_alpha}")
 
     # Fitting l1- and l2-regularized logistic regression models, using cross-validation to specify values
     # of regularization parameter
-    raise NotImplementedError()
+    for i in range(1, 3):
+        lambdas = np.linspace(0, 1, 6)
+        validation_scores = np.zeros(lambdas.shape[0])
+        for j, lam in enumerate(lambdas):
+            model = LogisticRegression(penalty=f"l{i}", lam=lam)
+            train_score, validation_score = cross_validate(model,
+                                                           X_train.to_numpy(),
+                                                           y_train.to_numpy(),
+                                                           misclassification_error)
+            validation_scores[j] = validation_score
+        best_lambda = lambdas[np.argmin(validation_scores)]
+        model = LogisticRegression(penalty=f"l{i}", lam=best_lambda)
+        model.fit(X_train.to_numpy(), y_train.to_numpy())
+        loss = model.loss(X_test.to_numpy(), y_test.to_numpy())
+        print(f"Best lambda for model with regularization L{i} is: {loss} with lambda {best_lambda}")
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    compare_fixed_learning_rates()
-    compare_exponential_decay_rates()
+    # compare_fixed_learning_rates()
+    # compare_exponential_decay_rates()
     fit_logistic_regression()
